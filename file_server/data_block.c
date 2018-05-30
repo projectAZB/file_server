e7e8d81a28d5885951f9e5c053616118033e8b05
Adam <abol@sas.upenn.edu>
2018-05-29 22:06:00 -0500

    add data block structure

---
+++ file_server/data_block.c
@@ -0,0 +1,9 @@
+//
+//  data_block.c
+//  file_server
+//
+//  Created by Adam on 5/29/18.
+//  Copyright © 2018 Adam. All rights reserved.
+//
+
+#include "data_block.h"
---
+++ file_server/data_block.h
@@ -0,0 +1,22 @@
+//
+//  data_block.h
+//  file_server
+//
+//  Created by Adam on 5/29/18.
+//  Copyright © 2018 Adam. All rights reserved.
+//
+
+#ifndef data_block_h
+#define data_block_h
+
+#include "constants.h"
+
+typedef unsigned char byte;
+
+typedef struct data_block_t {
+	byte bytes[MFS_BLOCK_SIZE];
+} data_block_t;
+
+typedef data_block_t * data_block_handle;
+
+#endif
