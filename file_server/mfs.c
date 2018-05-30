293b636aece641909ce9eadd34454d7fa9196f58
Adam <abol@sas.upenn.edu>
2018-05-29 22:54:34 -0500

    add mfs

--- file_server/mfs.c
+++ file_server/mfs.c
@@ -7,3 +7,19 @@
 //
 
 #include "mfs.h"
+#include <fcntl.h>
+#include <errno.h>
+
+int MFS_Init(char * hostname, int port)
+{
+	int open_status = open(hostname, O_RDWR);
+	if (open_status == -1) { //file image doesn't exist, create
+		printf("File Image doesn't exist");
+		return -1;
+	}
+	else { //file image does exist
+		printf("File Image does exist");
+		return 0;
+	}
+	
+}
--- file_server/mfs.h
+++ file_server/mfs.h
@@ -11,11 +11,12 @@
 
 #include <stdio.h>
 
-typedef struct MFS_Stat_t {
-	
-} MFS_Stat_t;
+#include "constants.h"
+
+//!!!before returning a success code, the file system should always fsync() the image!!!
 
 //takes a host name and port number and uses those to find the server exporting the file system.
+//0 on success, -1 on failure
 int MFS_Init(char * hostname, int port);
 
 //takes the parent inode number (which should be the inode number of a directory) and looks up the entry
@@ -25,7 +26,7 @@ int MFS_Lookup(int pinum, char * name);
 
 //returns some information about the file specified by inum. Upon success, return 0, otherwise -1. The
 //exact info returned is defined by MFS_Stat_t. Failure modes: inum does not exist.
-int MFS_Stat(int inum, MFS_Stat_t * m);
+int MFS_Stat(int inum, MFS_Stat_handle m);
 
 //writes a block of size 4096 bytes at the block offset specified by block . Returns 0 on success, -1 on
 //failure. Failure modes: invalid inum, invalid block, not a regular file (you can't write to directories).
