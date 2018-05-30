700f4879b1006a3c3b870c092c303b812a630af6
Adam <abol@sas.upenn.edu>
2018-05-29 22:55:01 -0500

    add more to constants

--- file_server/constants.h
+++ file_server/constants.h
@@ -2,7 +2,7 @@
 //  constants.h
 //  file_server
 //
-//  Created by Adam on 5/28/18.
+//  Created by Adam on 5/29/18.
 //  Copyright © 2018 Adam. All rights reserved.
 //
 
@@ -11,7 +11,30 @@
 
 #include <stdio.h>
 
+#define MFS_BLOCK_SIZE (int)4096
+
 #define NUM_INODES (int)4096
-#define NUM_DATA_BLOCKS (int)4096
+#define NUM_BLOCKS (int)4096
+
+#define CLIENT_TIMEOUT (int)5
+
+typedef enum file_t {
+	MFS_DIRECTORY = 0,
+	MFS_REGULAR_FILE = 1
+} file_t;
+
+typedef struct __MFS_Stat_t {
+	file_t type;   // MFS_DIRECTORY or MFS_REGULAR
+	size_t size;   // bytes
+	int blocks; // number of blocks allocated to file
+	// note: no permissions, access times, etc.
+} MFS_Stat_t;
+
+typedef MFS_Stat_t * MFS_Stat_handle;
+
+typedef struct __MFS_DirEnt_t {
+	int  inum;      // inode number of entry (-1 means entry not used)
+	char name[252]; // up to 252 bytes of name in directory (including \0)
+} MFS_DirEnt_t;
 
 #endif
