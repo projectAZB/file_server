596d13811d1e2cc3bdb68d0d4e8cd910d11c8ede
Adam <abol@sas.upenn.edu>
2018-05-31 17:59:01 -0500

    put structs back in constants

--- file_server/constants.h
+++ file_server/constants.h
@@ -23,18 +23,4 @@ typedef enum file_t {
 	MFS_REGULAR_FILE = 1
 } file_t;
 
-typedef struct __MFS_Stat_t {
-	file_t type;   // MFS_DIRECTORY or MFS_REGULAR
-	size_t size;   // bytes
-	int blocks; // number of blocks allocated to file
-	// note: no permissions, access times, etc.
-} MFS_Stat_t;
-
-typedef MFS_Stat_t * MFS_Stat_handle;
-
-typedef struct __MFS_DirEnt_t {
-	int  inum;      // inode number of entry (-1 means entry not used)
-	char name[252]; // up to 252 bytes of name in directory (including \0)
-} MFS_DirEnt_t;
-
 #endif
