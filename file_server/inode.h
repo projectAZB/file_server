dc40356b4a4d1832b91afd0f94e8d99ca97e3cd6
Adam <abol@sas.upenn.edu>
2018-05-31 03:05:45 -0500

    add new node variable, differentiate names, add parent self dirs, etc

---
+++ file_server/inode.h
@@ -0,0 +1,32 @@
+//
+//  inode.h
+//  file_server
+//
+//  Created by Adam on 5/30/18.
+//  Copyright © 2018 Adam. All rights reserved.
+//
+
+#ifndef inode_h
+#define inode_h
+
+typedef struct inode_t inode_t;
+
+struct inode_t {
+	int parentSelfDirs[2];
+	int inum;
+	char name[252];
+	file_t file_type;
+	size_t size;
+	//for directories
+	size_t dir_num_inodes;
+	int dir_child_inums[50];
+	//for regular files
+	size_t reg_num_blocks; //max is 10
+	size_t reg_block_offset;
+};
+
+void print_inode(inode_t inode) {
+	printf(" inum: %d\n name: %s\n file type: %d\n size: %lu\n num_inodes: %lu\n num_blocks: %lu\n block_offset: %lu\n", inode.inum, inode.name, inode.file_type, inode.size, inode.dir_num_inodes, inode.reg_num_blocks, inode.reg_block_offset);
+}
+
+#endif 
