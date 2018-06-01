eb55bc9e96a0a33716c170f97ce94a5e95b29a95
Adam <abol@sas.upenn.edu>
2018-06-01 09:38:46 -0500

    use class header file

--- file_server/mfs.h
+++ file_server/mfs.h
@@ -6,16 +6,17 @@
 //  Copyright © 2018 Adam. All rights reserved.
 //
 
-#ifndef mfs_h
-#define mfs_h
+#ifndef __MFS_h__
+#define __MFS_h__
 
-#include <stdio.h>
+#define MFS_DIRECTORY    (0)
+#define MFS_REGULAR_FILE (1)
 
-#include "constants.h"
+#define MFS_BLOCK_SIZE   (4096)
 
 typedef struct __MFS_Stat_t {
-	file_t type;   // MFS_DIRECTORY or MFS_REGULAR
-	size_t size;   // bytes
+	int type;   // MFS_DIRECTORY or MFS_REGULAR
+	int size;   // bytes
 	int blocks; // number of blocks allocated to file
 	// note: no permissions, access times, etc.
 } MFS_Stat_t;
@@ -25,39 +26,13 @@ typedef struct __MFS_DirEnt_t {
 	char name[252]; // up to 252 bytes of name in directory (including \0)
 } MFS_DirEnt_t;
 
-//!!!before returning a success code, the file system should always fsync() the image!!!
 
-//takes a host name and port number and uses those to find the server exporting the file system.
-//0 on success, -1 on failure
-int MFS_Init(char * hostname, int port);
+int MFS_Init(char *hostname, int port);
+int MFS_Lookup(int pinum, char *name);
+int MFS_Stat(int inum, MFS_Stat_t *m);
+int MFS_Write(int inum, char *buffer, int block);
+int MFS_Read(int inum, char *buffer, int block);
+int MFS_Creat(int pinum, int type, char *name);
+int MFS_Unlink(int pinum, char *name);
 
-//takes the parent inode number (which should be the inode number of a directory) and looks up the entry
-//name in it. The inode number of name is returned. Success: return inode number of name; failure: return
-//-1. Failure modes: invalid pinum, name does not exist in pinum.
-int MFS_Lookup(int pinum, char * name);
-
-//returns some information about the file specified by inum. Upon success, return 0, otherwise -1. The
-//exact info returned is defined by MFS_Stat_t. Failure modes: inum does not exist.
-int MFS_Stat(int inum, MFS_Stat_t * m);
-
-//writes a block of size 4096 bytes at the block offset specified by block . Returns 0 on success, -1 on
-//failure. Failure modes: invalid inum, invalid block, not a regular file (you can't write to directories).
-int MFS_Write(int inum, char * buffer, int block);
-
-//reads a block specified by block into the buffer from file specified by inum . The routine should work
-//for either a file or directory; directories should return data in the format specified by MFS_DirEnt_t.
-//Success: 0, failure: -1. Failure modes: invalid inum, invalid block.
-int MFS_Read(int inum, char * buffer, int block);
-
-//makes a file ( type == MFS_REGULAR_FILE) or directory ( type == MFS_DIRECTORY) in the parent directory
-//specified by pinum of name name . Returns 0 on success, -1 on failure. Failure modes: pinum does not
-//exist. If name already exists, return success (think about why).
-int MFS_Creat(int pinum, int type, char * name);
-
-//removes the file or directory name from the directory specified by pinum . 0 on success, -1 on failure.
-//Failure modes: pinum does not exist, pinum does not represent a directory, the to-be-unlinked directory
-//is NOT empty. Note that the name not existing is NOT a failure by our definition (think about why this
-//might be).
-int MFS_Unlink(int pinum, char * name);
-
-#endif 
+#endif // __MFS_h__
