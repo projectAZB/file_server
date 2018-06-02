058bebb4b5941a6f91062ae522fe57a6194bb7e8
Adam <abol@sas.upenn.edu>
2018-06-01 20:47:04 -0500

    get test 5 passing

--- file_server/mfs.c
+++ file_server/mfs.c
@@ -34,7 +34,7 @@ void init_reg_inode(inode_t * inode, int inum, int parent, int self, char * name
 	inode->reg_num_blocks = 0;
 	inode->reg_block_offset = -1;
 	inode->dir_num_inodes = 0; //set 0 to start
-	memset(inode->dir_child_inums, -1, 50); //-1 all entries
+	memset(inode->dir_child_inums, -1, MAX_INODES); //-1 all entries
 }
 
 void init_dir_inode(inode_t * inode, int inum, int parent, int self, char * name)
@@ -46,7 +46,7 @@ void init_dir_inode(inode_t * inode, int inum, int parent, int self, char * name
 	strncpy(inode->name, name, strlen(name) + 1);
 	inode->file_type = MFS_DIRECTORY;
 	inode->dir_num_inodes = 0; //set 0 to start
-	memset(inode->dir_child_inums, -1, 50); //-1 all entries
+	memset(inode->dir_child_inums, -1, MAX_INODES); //-1 all entries
 	inode->reg_block_offset = -1;
 	inode->reg_num_blocks = 0;
 }
@@ -55,7 +55,6 @@ int MFS_Init(char * hostname, int port)
 {
 	_file_image_fd = open(hostname, O_RDWR);
 	if (_file_image_fd == -1) {
-		printf("File doesn't exist\n");
 		_file_image_fd = open(hostname, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 		if (_file_image_fd == -1) { //error creating the file
 			return -1;
@@ -116,10 +115,10 @@ int r_search(inode_t inode, int pinum, char * name)
 			for (int i = 0; i < inode.dir_num_inodes; i++)
 			{
 				int child_inum = inode.dir_child_inums[i];
-				inode_t child_inode = _inode_table.inodes[child_inum];
-				if (child_inode.inum == -1) {
+				if (_inode_table.inode_bitmap[child_inum] <= 0) {
 					continue;
 				}
+				inode_t child_inode = _inode_table.inodes[child_inum];
 				if (strncmp(child_inode.name, name, strlen(name)) == 0) { //correct filename
 					return child_inode.inum;
 				}
@@ -131,10 +130,10 @@ int r_search(inode_t inode, int pinum, char * name)
 		for (int i = 0; i < inode.dir_num_inodes; i++)
 		{
 			int child_inum = inode.dir_child_inums[i];
-			inode_t child_inode = _inode_table.inodes[child_inum];
-			if (child_inode.inum == -1) {
+			if (_inode_table.inode_bitmap[child_inum] <= 0) {
 				continue;
 			}
+			inode_t child_inode = _inode_table.inodes[child_inum];
 			if (child_inode.file_type == MFS_DIRECTORY) { //if directory, recursively search
 				return r_search(child_inode, pinum, name);
 			}
@@ -154,6 +153,24 @@ int MFS_Lookup(int pinum, char * name)
 		return -1;
 	}
 	
+	if (_inode_table.inode_bitmap[pinum] <= 0) {
+		return -1;
+	}
+	
+	if (strcmp(name, "..") == 0) { //parent directory
+		inode_t parent = _inode_table.inodes[pinum];
+		//get the parent inum, then inode
+		int parent_inum = parent.parentSelfDirs[0];
+		return parent_inum;
+	}
+	
+	if (strcmp(name, ".") == 0) { //self directory
+		inode_t parent = _inode_table.inodes[pinum];
+		//get the self inum, then inode
+		int self_inum = parent.parentSelfDirs[1];
+		return self_inum;
+	}
+	
 	inode_t root = _inode_table.inodes[0];
 	int ret = r_search(root, pinum, name);
 	return ret;
@@ -165,6 +182,10 @@ int MFS_Stat(int inum, MFS_Stat_t * m)
 		return -1;
 	}
 	
+	//if it was deleted or never created, return error
+	if (_inode_table.inode_bitmap[inum] <= 0) {
+		return -1;
+	}
 	inode_t inode = _inode_table.inodes[inum];
 	if (inode.file_type == MFS_DIRECTORY) {
 		m->blocks = 1;
@@ -186,21 +207,23 @@ int MFS_Creat(int pinum, int type, char * name)
 		return -1;
 	}
 	
-	int lookup_inum = MFS_Lookup(pinum, name);
+	if (_inode_table.inode_bitmap[pinum] == -1) {
+		return -1;
+	}
 	inode_t parent_inode = _inode_table.inodes[pinum];
 	if (parent_inode.file_type == MFS_REGULAR_FILE) { //can't add children to a regular file
 		return -1;
 	}
-	if (lookup_inum == -1) { //file doesn't exist, create it
+	if (_inode_table.inode_bitmap[pinum] == 0) { //file doesn't exist, create it
 		parent_inode.dir_child_inums[parent_inode.dir_num_inodes] = _inode_table.next_free;
-		parent_inode.dir_num_inodes++;
+		(parent_inode.dir_num_inodes)++;
 		_inode_table.inodes[parent_inode.inum] = parent_inode;
 		if (type == MFS_DIRECTORY) {
 			inode_t dir_inode;
 			init_dir_inode(&dir_inode, _inode_table.next_free, pinum, _inode_table.next_free, name);
 			_inode_table.inode_bitmap[_inode_table.next_free] = 1;
 			_inode_table.inodes[_inode_table.next_free] = dir_inode;
-			_inode_table.next_free++;
+			(_inode_table.next_free)++;
 			
 		}
 		else { //type == MFS_REG_FILE
@@ -208,7 +231,7 @@ int MFS_Creat(int pinum, int type, char * name)
 			init_reg_inode(&reg_inode, _inode_table.next_free, pinum, _inode_table.next_free, name);
 			_inode_table.inode_bitmap[_inode_table.next_free] = 1;
 			_inode_table.inodes[_inode_table.next_free] = reg_inode;
-			_inode_table.next_free++;
+			(_inode_table.next_free)++;
 		}
 		
 		//go to beginning of file, where the inode table is stored
@@ -225,7 +248,7 @@ int MFS_Creat(int pinum, int type, char * name)
 		
 		return 0;
 	}
-	else { //file does exist, return a success
+	else { //file does exist, return a success _inode_table.inode_bitmap[pinum] == 1
 		return 0;
 	}
 }
@@ -236,6 +259,9 @@ int MFS_Write(int inum, char * buffer, int block)
 		return -1;
 	}
 	
+	if (_inode_table.inode_bitmap[inum] <= 0) { //if not allocated or unlinked, return -1
+		return -1;
+	}
 	//get the inode
 	inode_t inode_to_write = _inode_table.inodes[inum];
 	if (inode_to_write.file_type == MFS_DIRECTORY) {
@@ -295,7 +321,12 @@ int MFS_Read(int inum, char * buffer, int block)
 	if (block > 9 || block < 0 || inum < 0 || inum >= _inode_table.next_free) {
 		return -1;
 	}
+	
+	if (_inode_table.inode_bitmap[inum] <= 0) { //check if unlinked or uncreated
+		return -1;
+	}
 	inode_t inode = _inode_table.inodes[inum];
+	
 	if (inode.file_type == MFS_DIRECTORY) {
 		MFS_DirEnt_t dir;
 		dir.inum = inode.inum;
@@ -319,6 +350,9 @@ int MFS_Unlink(int pinum, char *name)
 		return -1;
 	}
 	
+	if (_inode_table.inode_bitmap[pinum] <= 0) {
+		return -1;
+	}
 	inode_t inode = _inode_table.inodes[pinum];
 	
 	if (inode.file_type == MFS_REGULAR_FILE) { // has to be a directory
@@ -335,9 +369,9 @@ int MFS_Unlink(int pinum, char *name)
 	{
 		inode_t child = _inode_table.inodes[inode.dir_child_inums[i]];
 		if (strcmp(child.name, name) == 0) { // name matches
-			_inode_table.inode_bitmap[child.inum] = -1;
-			inode.dir_child_inums[i] = -1;
-			_inode_table.inodes[inode.dir_child_inums[i]] = inode;
+			_inode_table.inode_bitmap[child.inum] = -1; //remove the inode from the inode bitmap
+			child.inum = -1;
+			_inode_table.inodes[inode.dir_child_inums[i]] = child;
 		}
 	}
 	
